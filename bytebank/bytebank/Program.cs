0f065b7899b5c25656f63f3990f00ba4ba6cf32a
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-28 12:24:46 -0300

    Adicionando a classe ContaCorrente

--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -1,15 +1,18 @@
-﻿Console.WriteLine("Boas vindas ao seu banco, ByteBank!");
+﻿using bytebank;
 
-string titular = "Davi Kennedy";
-string conta = "10123-X";
-int numeroAgencia = 23;
-string nomeAgencia = "Agência Central";
-double saldo = 237.50;
+Console.WriteLine("Boas vindas ao seu banco, ByteBank!");
 
-Console.WriteLine($"Titular: {titular}");
-Console.WriteLine($"Conta: {conta}");
-Console.WriteLine($"Número da agência: {numeroAgencia}");
-Console.WriteLine($"Nome da agência: {nomeAgencia}");
-Console.WriteLine($"Saldo: {saldo}");
+ContaCorrente conta1 = new ContaCorrente();
+conta1.titular = "Davi Kennedy";
+conta1.conta = "10123-X";
+conta1.numeroAgencia = 23;
+conta1.nomeAgencia = "Agência Central";
+conta1.saldo = 237.50;
+
+Console.WriteLine($"Titular: {conta1.titular}");
+Console.WriteLine($"Conta: {conta1.conta}");
+Console.WriteLine($"Número da agência: {conta1.numeroAgencia}");
+Console.WriteLine($"Nome da agência: {conta1.nomeAgencia}");
+Console.WriteLine($"Saldo: {conta1.saldo}");
 
 Console.ReadKey();
