3185a868e08300126a749bb8d17d7722f89d7258
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-11-28 10:35:36 -0300

    Criando as bases do projeto

--- bytebank/bytebank/Program.cs
+++ bytebank/bytebank/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Boas vindas ao seu banco, ByteBank!");
+
+string titular = "Davi Kennedy";
+string conta = "10123-X";
+int numeroAgencia = 23;
+string nomeAgencia = "Agência Central";
+double saldo = 237.50;
+
+Console.WriteLine($"Titular: {titular}");
+Console.WriteLine($"Conta: {conta}");
+Console.WriteLine($"Número da agência: {numeroAgencia}");
+Console.WriteLine($"Nome da agência: {nomeAgencia}");
+Console.WriteLine($"Saldo: {saldo}");
+
+Console.ReadKey();
